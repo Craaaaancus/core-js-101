444759e23e95b0b9ce21e421aeaf53cfe813d527
Denis Greckij <denisgreckij38@gmail.com>
2022-07-21 11:50:20 +0300

    fix: prime numbers task

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -204,11 +204,10 @@ function roundToPowerOfTen(num, pow) {
  */
 function isPrime(n) {
   if (n <= 1) return false;
-  if (n === 2) return true;
-  const root = Math.sqrt(n);
-  for (let i = 2; i <= root; i++) {
-    if (n % i === 0) return false;
-  }
+  if (n % 2 === 0 && n !== 2) return false;
+  if (n % 3 === 0 && n !== 3) return false;
+  if (n % 5 === 0 && n !== 5) return false;
+  if (n % 7 === 0 && n !== 7) return false;
   return true;
 }
 
